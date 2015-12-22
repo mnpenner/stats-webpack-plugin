be76fba43be3c9b6d7e241775b9da9403a248bcc
mnpenner <marknpenner@gmail.com>
2015-12-22 09:43:38 -0800

    Added forceWrite option

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var path = require('path');
+
 /**
  * Create a new StatsPlugin that causes webpack to generate a stats file as
  * part of the emitted assets.
@@ -6,25 +9,30 @@
  * @param {Object} options Options passed to the stats' `.toJson()`.
  */
 function StatsPlugin (output, options) {
-  this.output = output
-  this.options = options
+  this.output = output;
+  this.options = options;
 }
 
 StatsPlugin.prototype.apply = function apply (compiler) {
-  var output = this.output
-  var options = this.options
+  var output = this.output;
+  var options = this.options;
 
   compiler.plugin('emit', function onEmit (compilation, done) {
-    compilation.assets[output] = {
-      size: function getSize () {
-        return 0
-      },
-      source: function getSource () {
-        return JSON.stringify(compilation.getStats().toJson(options))
-      }
+    var json = JSON.stringify(compilation.getStats().toJson(options));
+    if(options.forceWrite) {
+      fs.writeFile(path.resolve(compiler.context, compiler.options.output.path, output), json, done);
+    } else {
+      compilation.assets[output] = {
+        size: function getSize() {
+          return Buffer.byteLength(json, 'utf8');
+        },
+        source: function getSource() {
+          return json;
+        }
+      };
+      done();
     }
-    done()
   })
-}
+};
 
-module.exports = StatsPlugin
+module.exports = StatsPlugin;
